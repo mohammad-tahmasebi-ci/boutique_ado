e32e2c0c1a6f4982257431e5182e452abe376249
mohammad-tahmasebi-ci <mohammad.tahmasebi@students.codeinstitute.net>
2025-05-28 15:52:12 +0100

    add checkout success logic

--- checkout/signals.py
+++ checkout/signals.py
@@ -13,7 +13,7 @@ def update_on_save(sender, instance, created, **kwargs):
 
 
 @receiver(post_save, sender=OrderLineItem)
-def update_on_save(sender, instance, created, **kwargs):
+def update_on_delete(sender, instance, created, **kwargs):
     """
     update order total on lineitem delete
     """
