4cb2502796d1b4cb102bf73c700b5c0aed9d0dd1
mohammad-tahmasebi-ci <mohammad.tahmasebi@students.codeinstitute.net>
2025-06-05 21:02:28 +0100

    add settings to configure as DEVELOPMENT environment for debug

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '!`C/9/DTR8*D~hS'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
     '127.0.0.1',  # VS code preview
